56005b1777b40948e94fe4ee24095b85787ebbea
Rafal <rf.zajac@gmail.com>
2023-08-15 17:19:56 +0200

    content page made with DOM methods, and button has a even listener now

--- data.js
+++ data.js
@@ -1,5 +1,6 @@
 var data = {
-    "photos": [{
+    "photos": [
+        {
         "id": 228,
         "sol": 666,
         "camera": {
@@ -11,39 +12,40 @@ var data = {
         "img_src": "http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/00666/opgs/edr/fcam/FRB_456612388EDR_F0361146FHAZ00323M_.JPG",
         "earth_date": "2014-06-21",
         "rover": {
-            "id": 5,
-            "name": "Curiosity",
-            "landing_date": "2012-08-06",
-            "launch_date": "2011-11-26",
-            "status": "active",
-            "max_sol": 3912,
-            "max_date": "2023-08-08",
-            "total_photos": 670459,
-            "cameras": [
-                {
-                    "name": "FHAZ",
-                    "full_name": "Front Hazard Avoidance Camera"
-                }, {
-                    "name": "NAVCAM",
-                    "full_name": "Navigation Camera"
-                }, {
-                    "name": "MAST",
-                    "full_name": "Mast Camera"
-                }, {
-                    "name": "CHEMCAM",
-                    "full_name": "Chemistry and Camera Complex"
-                }, {
-                    "name": "MAHLI",
-                    "full_name": "Mars Hand Lens Imager"
-                }, {
-                    "name": "MARDI",
-                    "full_name": "Mars Descent Imager"
-                }, {
-                    "name": "RHAZ",
-                    "full_name": "Rear Hazard Avoidance Camera"
-                }]
-        }
-    },      {
+        "id": 5,
+        "name": "Curiosity",
+        "landing_date": "2012-08-06",
+        "launch_date": "2011-11-26",
+        "status": "active",
+        "max_sol": 3912,
+        "max_date": "2023-08-08",
+        "total_photos": 670459,
+        "cameras": [
+            {
+                "name": "FHAZ",
+                "full_name": "Front Hazard Avoidance Camera"
+            }, {
+                "name": "NAVCAM",
+                "full_name": "Navigation Camera"
+            }, {
+                "name": "MAST",
+                "full_name": "Mast Camera"
+            }, {
+                "name": "CHEMCAM",
+                "full_name": "Chemistry and Camera Complex"
+            }, {
+                "name": "MAHLI",
+                "full_name": "Mars Hand Lens Imager"
+            }, {
+                "name": "MARDI",
+                "full_name": "Mars Descent Imager"
+            }, {
+                "name": "RHAZ",
+                "full_name": "Rear Hazard Avoidance Camera"
+            }]
+         }
+        },
+        {
         "id": 5853,
         "sol": 666,
         "camera": {
@@ -55,41 +57,41 @@ var data = {
         "img_src": "http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/00666/opgs/edr/fcam/FLB_456612388EDR_F0361146FHAZ00323M_.JPG",
         "earth_date": "2014-06-21",
         "rover": {
-            "id": 5,
-            "name": "Curiosity",
-            "landing_date": "2012-08-06",
-            "launch_date": "2011-11-26",
-            "status": "active",
-            "max_sol": 3912,
-            "max_date": "2023-08-08",
-            "total_photos": 670459,
-            "cameras": [
-                {
-                    "name": "FHAZ",
-                    "full_name": "Front Hazard Avoidance Camera"
-                }, {
-                    "name": "NAVCAM",
-                    "full_name": "Navigation Camera"
-                }, {
-                    "name": "MAST",
-                    "full_name": "Mast Camera"
-                }, {
-                    "name": "CHEMCAM",
-                    "full_name": "Chemistry and Camera Complex"
-                }, {
-                    "name": "MAHLI",
-                    "full_name": "Mars Hand Lens Imager"
-                }, {
-                    "name": "MARDI",
-                    "full_name": "Mars Descent Imager"
-                }, {
-                    "name": "RHAZ",
-                    "full_name": "Rear Hazard Avoidance Camera"
-                }]
-        }
-        }]
+        "id": 5,
+        "name": "Curiosity",
+        "landing_date": "2012-08-06",
+        "launch_date": "2011-11-26",
+        "status": "active",
+        "max_sol": 3912,
+        "max_date": "2023-08-08",
+        "total_photos": 670459,
+        "cameras": [
+            {
+                "name": "FHAZ",
+                "full_name": "Front Hazard Avoidance Camera"
+            }, {
+                "name": "NAVCAM",
+                "full_name": "Navigation Camera"
+            }, {
+                "name": "MAST",
+                "full_name": "Mast Camera"
+            }, {
+                "name": "CHEMCAM",
+                "full_name": "Chemistry and Camera Complex"
+            }, {
+                "name": "MAHLI",
+                "full_name": "Mars Hand Lens Imager"
+            }, {
+                "name": "MARDI",
+                "full_name": "Mars Descent Imager"
+            }, {
+                "name": "RHAZ",
+                "full_name": "Rear Hazard Avoidance Camera"
+            }]
+    }
+    }, 
+]
 }
 
-let photo1 = data["photos"][0]["img_src"];
-let photo2 = data["photos"][0]["img_src"];
+
 
--- main.js
+++ main.js
@@ -1,9 +1,75 @@
-let picContainer = document.getElementById("api-data");
 
-const photos = [photo1, photo2];
+// * Generate photos on a webpage
+function showPhotos() {
 
-for (let i = 0; i < photos.length; i++) {
-    let pic = document.createElement('img');
-    pic.setAttribute('src', photos[i])
-    picContainer.appendChild(pic);
+    
+    const photoDiv = document.getElementById("photo-gallery");
+
+     // *Create a div containing cards group
+    const cardGroup = document.createElement('div');
+    cardGroup.setAttribute("class", "row row-cols-1 row-cols-md-3 g-4");
+    photoDiv.appendChild(cardGroup);
+
+
+    for (let i = 0; i < data.photos.length; i++) {
+
+        // *Create a card columns and card body
+        const colCard = document.createElement('div');
+        colCard.setAttribute('class', 'col');
+        cardGroup.appendChild(colCard);
+        
+        const cardBody = document.createElement('div');
+        cardBody.setAttribute('class', 'card h-100');
+        colCard.appendChild(cardBody);
+
+        // *Create card body elements
+        const cardPhoto = document.createElement('img');
+        cardPhoto.setAttribute('class', 'card-img-top');
+        cardPhoto.setAttribute('src', data.photos[i].img_src);
+        cardPhoto.setAttribute('alt', "Made on: " + data.photos[i].earth_date);
+        cardBody.appendChild(cardPhoto);
+
+        const photoDesc = document.createElement('ul');
+        photoDesc.setAttribute('class', 'list-group list-group-flush');
+        cardBody.appendChild(photoDesc);
+
+        const roverLi = document.createElement('li');
+        roverLi.setAttribute('class', 'list-group-item');
+        roverLi.innerHTML = "<strong>Rover : </strong>" + data.photos[i].rover.name;
+        photoDesc.appendChild(roverLi);
+
+        const solLi = document.createElement('li');
+        solLi.setAttribute('class', 'list-group-item');
+        solLi.innerHTML = "<strong>Solar day : </strong>" + data.photos[i].sol;
+        photoDesc.appendChild(solLi);
+
+        const idLi = document.createElement('li');
+        idLi.setAttribute('class', 'list-group-item');
+        idLi.innerHTML = "<strong>Photo ID : </strong>" + data.photos[i].id;
+        photoDesc.appendChild(idLi);
+
+        const camLi = document.createElement('li');
+        camLi.setAttribute('class', 'list-group-item');
+        camLi.innerHTML = "<strong>Camera : </strong>" + data.photos[i].camera.name;
+        photoDesc.appendChild(camLi);
+
+        // *Create a card footer
+        const cardFooter = document.createElement('div');
+        cardFooter.setAttribute('class', 'card-footer');
+        const footerContent = document.createElement('small');
+        footerContent.setAttribute('class', 'text-body-secondary');
+        footerContent.innerHTML = "Earth date : " + data.photos[i].earth_date;
+        cardBody.appendChild(cardFooter);
+        cardFooter.appendChild(footerContent);
+    }
 }
+
+// * Add an event listener
+function galleryListener() {
+    const displayButton = document.getElementById("show-photos-button");
+    displayButton.addEventListener('click', showPhotos);
+}
+
+// * Call the gallery button listener function
+galleryListener();
+
