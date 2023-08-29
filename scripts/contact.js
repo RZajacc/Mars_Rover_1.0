d75df2a4e0da8892359f6648e08d74d227871471
Rafal <rf.zajac@gmail.com>
2023-08-29 13:13:33 +0200

    cleaned up a bit html files

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,21 +1,20 @@
-
 // *Initialize emailJS
 (function () {
-    emailjs.init("DzZ6BIu3N4pT6e34Q");
+  emailjs.init("DzZ6BIu3N4pT6e34Q");
 })();
 
-const contact_Form = document.getElementById('contact-form');
+const contact_Form = document.getElementById("contact-form");
 
-contact_Form.addEventListener('submit', (event) => {
-    
-    // handle the form data
-    event.preventDefault();
-    
-    // *Send form with emailjs
-    emailjs.sendForm('contact_service', 'contact_form', contact_Form)
-        .then(() => contact_Form.submit())
-        ,(error) => console.log('FAILED....', error);    
+// * Send email with data from the document
+contact_Form.addEventListener("submit", (event) => {
+  // handle the form data
+  event.preventDefault();
+
+  // *Send form with emailjs
+  emailjs
+    .sendForm("contact_service", "contact_form", contact_Form)
+    .then(() => contact_Form.submit())
+    .catch((error) => {
+      console.log("FAILED....", error);
+    });
 });
-
-
-
